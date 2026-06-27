5044a2088b0c92bf62c0ecae33c4da00b1d9a29a
hyungjun123 <ksi26792@gmail.com>
2026-06-27 19:45:46 +0900

    change Kim score to 100

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 students = [
-    {"name": "Kim", "score": 90},
+    {"name": "Kim", "score": 100}, #95?90?->100 update
     {"name": "Lee", "score": 80},
     {"name" : "Jee", "score" : 70} #new data
 ]
